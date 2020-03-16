e0ac929dbc2ab42a8255c0475ee54c5dd5b889c3
ZidaneDC <azyzadc@gmail.com>
2020-03-16 15:41:40 -0700

    Finished Version
    Finished version of project 2. Uses game over and pause screens with resume and restart buttons.

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, speed); //x is 0 because bullet moves in one direction, speed is speed you enter in the unity editor
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if bullet has gone beyond screen bounds, if it has, destroy it to save space
+        if (Camera.main.WorldToViewportPoint(transform.position).y > 1) //unity knows that if you only have one camera, that is your main camera
+            //world to view point = what the camera sees, basically this is converting bullets position to position to camera, and if its too far, delete it
+            Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); //allows to access players physics
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal"); //these are 0 or 1 values, yes or no, 0 not moving and not pressing a button, 1 is pressing WASD and moving
+        float y = Input.GetAxisRaw("Vertical");
+
+        float moveX = x * speed;
+        float moveY = y * speed;
+
+        rb.velocity = new Vector2(moveX, moveY); //speed is applied as a velocity to their physics
+    }
+}
